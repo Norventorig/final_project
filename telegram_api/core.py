0d4255be21f74df3a5a431bdc14d6091af0f599b
ордей абуз <gordlab@mail.ru>
2025-02-10 15:54:39 +1000

    adjusts to security changes

--- telegram_api/core.py
+++ telegram_api/core.py
@@ -1,4 +1,10 @@
-from telegram_api.utils.bot_handler import BotHandler, tg_bot
+import telebot
+from settings import site_settings
+from site_api.core import site_api_handler
+from telegram_api.utils.bot_handler import BotHandler
 
 
-bot_handler = BotHandler(tg_bot)
+tg_bot = telebot.TeleBot(site_settings.tg_token.get_secret_value())
+bot_handler = BotHandler(tg_bot,
+                         series_by_id_function=site_api_handler.get_series_by_id,
+                         movie_by_id_function=site_api_handler.get_movie_by_id)
