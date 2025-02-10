30d8f7af945747fd01cc9bd8adcf22f18f575e49
ордей абуз <gordlab@mail.ru>
2025-02-10 16:48:14 +1000

    adjusts to security changes

--- site_api/core.py
+++ site_api/core.py
@@ -1,5 +1,10 @@
+from settings import site_settings
 from site_api.utils.site_api_handler import SiteAPIHandler
 
 
-site_api_handler = SiteAPIHandler
-
+site_api_handler = SiteAPIHandler(host=site_settings.rapidapi_host,
+                                  key=site_settings.rapidapi_key,
+                                  get_100_movies_url=site_settings.base_url,
+                                  get_100_series_url=site_settings.series,
+                                  get_movie_by_id_url=site_settings.movie_data,
+                                  get_series_by_id_url=site_settings.series_data)
