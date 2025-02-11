2311a218c27e7ab0b80dc5d5fc3243e1bb08c4f2
ордей абуз <gordlab@mail.ru>
2025-02-11 19:02:10 +1000

    fixes error with get_secret_value

--- site_api/core.py
+++ site_api/core.py
@@ -2,9 +2,9 @@ from settings import site_settings
 from site_api.utils.site_api_handler import SiteAPIHandler
 
 
-site_api_handler = SiteAPIHandler(host=site_settings.rapidapi_host,
-                                  key=site_settings.rapidapi_key,
-                                  get_100_movies_url=site_settings.base_url,
-                                  get_100_series_url=site_settings.series,
-                                  get_movie_by_id_url=site_settings.movie_data,
-                                  get_series_by_id_url=site_settings.series_data)
+site_api_handler = SiteAPIHandler(host=site_settings.rapidapi_host.get_secret_value(),
+                                  key=site_settings.rapidapi_key.get_secret_value(),
+                                  get_100_movies_url=site_settings.base_url.get_secret_value(),
+                                  get_100_series_url=site_settings.series.get_secret_value(),
+                                  get_movie_by_id_url=site_settings.movie_data.get_secret_value(),
+                                  get_series_by_id_url=site_settings.series_data.get_secret_value())
