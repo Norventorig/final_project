1b52843ee4d774048c29287c7ccd9da5b0e5c519
ордей абуз <gordlab@mail.ru>
2025-02-11 19:02:46 +1000

    expands params for BotHandler

--- telegram_api/core.py
+++ telegram_api/core.py
@@ -2,9 +2,14 @@ import telebot
 from settings import site_settings
 from site_api.core import site_api_handler
 from telegram_api.utils.bot_handler import BotHandler
+from database.core import crud, db, User, History
 
 
 tg_bot = telebot.TeleBot(site_settings.tg_token.get_secret_value())
 bot_handler = BotHandler(tg_bot,
                          series_by_id_function=site_api_handler.get_series_by_id,
-                         movie_by_id_function=site_api_handler.get_movie_by_id)
+                         movie_by_id_function=site_api_handler.get_movie_by_id,
+                         crud=crud,
+                         db=db,
+                         user_model=User,
+                         history_model=History)
